2c4c52e3657a1f8122e719dcdd16e55fa40c0014
Samra Kasim <skasim@gmail.com>
2023-12-11 15:51:40 +0100

    add modified files

---
+++ chitchat/kafka_producer.py
@@ -0,0 +1,35 @@
+from confluent_kafka import Producer
+import uuid
+import socket
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+p = Producer({'bootstrap.servers': os.getenv("KAFKA_BOOTSTAP_SERVER_EXTERNAL"),
+        'client.id': socket.gethostname()})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+with open("data/dummy_chat_data.json") as f:
+    data = json.load(f)
+    for i, item in enumerate(data):
+        print(f"processing doc: {i}: {item}")
+        # Trigger any available delivery report callbacks from previous produce() calls
+        p.poll(0)
+
+        # Asynchronously produce a message. The delivery report callback will
+        # be triggered from the call to poll() above, or flush() below, when the
+        # message has been successfully delivered or failed permanently.
+        p.produce(os.getenv("KAFKA_TOPIC_CHITCHAT"), json.dumps(item), callback=delivery_report)
+
+
+p.flush()
